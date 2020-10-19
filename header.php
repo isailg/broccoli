fd7ff546d4816cc567f87080a56f1a4270e3f27d
isailg <ilogamx@gmail.com>
2020-10-19 14:07:10 -0500

    Header added

--- header.php
+++ header.php
@@ -7,3 +7,24 @@
     <?php wp_head() ?>
   </head>
   <body>
+  <header>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-4">
+                <img src="<?php echo get_template_directory_uri()?>/assets/images/logo.jpeg" alt="logo">
+            </div>
+            <div class="col-8">
+                <nav>
+                    <?php wp_nav_menu(
+                        array(
+                            'theme_location' => 'top_menu',
+                            'menu_class'    => 'menu-principal',
+                            'container_class' => 'container-menu',
+                        )
+                    );
+                    ?>
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
